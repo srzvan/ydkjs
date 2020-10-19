53e0ebb9505dfd89785a1ad61fe4abff177384fc
srzvan <sbingu.razvan@gmail.com>
2020-10-19 15:08:22 +0300

    added cache to isPrime

---
+++ scope&closure/closure-1.js
@@ -0,0 +1,46 @@
+var isPrime = (function isPrimeWithCache() {
+  var cache = {};
+
+  function isPrime(v) {
+    if (v <= 3) {
+      return v > 1;
+    }
+
+    if (v % 2 == 0 || v % 3 == 0) {
+      return false;
+    }
+
+    var vSqrt = Math.sqrt(v);
+
+    for (let i = 5; i <= vSqrt; i += 6) {
+      if (!(v in cache)) {
+        if (v % i == 0 || v % (i + 2) == 0) {
+          cache[v] = false;
+          break;
+        }
+      }
+    }
+
+    if (!(v in cache)) {
+      cache[v] = true;
+    }
+
+    return cache[v];
+  }
+
+  return isPrime;
+})();
+
+function factorize(v) {
+  if (!isPrime(v)) {
+    let i = Math.floor(Math.sqrt(v));
+
+    while (v % i != 0) {
+      i--;
+    }
+
+    return [...factorize(i), ...factorize(v / i)];
+  }
+
+  return [v];
+}
