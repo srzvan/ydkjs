4c5c21b73f1ef671ca9b3a9d0cb8290f3d88af6c
srzvan <sbingu.razvan@gmail.com>
2020-10-29 20:58:01 +0200

    implementation for the calculator function

--- scope&closure/closure-3.js
+++ scope&closure/closure-3.js
@@ -1,3 +1,88 @@
+var calculator = calculator();
+
+function calculator() {
+  var input = [];
+
+  return function calc(key) {
+    var lastInputValue = input[input.length - 1];
+
+    if (isInvalidInput(key)) {
+      return undefined;
+    }
+
+    if (isNumber(key)) {
+      if (isNumber(lastInputValue)) {
+        input.splice(input.length - 1, 1, toNumber(lastInputValue + key));
+      } else if (lastInputValue == "=") {
+        input.splice(0, input.length, toNumber(key));
+      } else {
+        input.push(toNumber(key));
+      }
+    } else if (key == "=") {
+      const total = equals();
+      input.splice(0, input.length, total, "=");
+
+      return formatTotal(total);
+    } else {
+      input.push(key);
+    }
+
+    return key;
+  };
+
+  function equals() {
+    var total = 0;
+    var currentOperation = "";
+
+    for (let val of input) {
+      if (isNumber(val)) {
+        if (currentOperation) {
+          switch (currentOperation) {
+            case "+":
+              total += val;
+              break;
+            case "-":
+              total -= val;
+              break;
+            case "*":
+              total *= val;
+              break;
+            case "/":
+              total /= val;
+              break;
+            default:
+              break;
+          }
+
+          continue;
+        }
+
+        total += val;
+      } else {
+        currentOperation = val;
+      }
+    }
+
+    return total;
+  }
+
+  function toNumber(val) {
+    return Number.parseInt(val);
+  }
+
+  function isNumber(val) {
+    return !Object.is(Number.parseInt(val), NaN);
+  }
+
+  function isInvalidInput(k) {
+    var operations = ["+", "-", "/", "*", "="];
+
+    return (
+      (input.length == 0 && !isNumber(k)) || (operations.indexOf(k) == -1 && !isNumber(k)) || Number.parseInt(k) >= 10
+    );
+  }
+}
+
 function useCalc(calc, keys) {
   return [...keys].reduce(function showDisplay(display, key) {
     var ret = String(calc(key));
